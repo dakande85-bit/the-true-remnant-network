d42976fa318371ff82d393e027f43dbccfc93dd1
dakande85-bit <dakande85@gmail.com>
2026-06-22 17:16:42 +0100

    Load True Remnant logo stylesheet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./overrides.css";
+import "./logo.css";
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 
